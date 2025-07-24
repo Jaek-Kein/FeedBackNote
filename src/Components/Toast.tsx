5b842f12590f91ffe1556a36d4986b60139e9450
Jaewook Kim / Kein <yearsingle@naver.com>
2025-07-24 10:21:04 +0900

    fix: Song Input Limitation

---
+++ src/Components/Toast.tsx
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+
+const ToastPopup = styled.div<{ toast: boolean }>`
+  position: fixed;
+  left: 50%;
+  top: 0;
+  transform: ${({ toast }) =>
+    toast ? "translate(-50%, 50%)" : "translate(-50%, -250%)"};
+  transition: transform 0.2s ease-in-out;
+
+  width: fit-content;
+  height: 22px;
+  box-sizing: border-box;
+  padding: 20px 20px;
+  background-color: #000000dd;
+  backdrop-filter: blur(20px);
+  border: 1px solid gray;
+  color: white;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-family: Pretendard;
+  z-index: 100000;
+`;
+
+interface Props {
+  message: string;
+  visible: boolean;
+  onhide: () => void;
+}
+
+const Toast: React.FC<Props> = ({message , visible, onhide}) => {
+
+  useEffect(() => {
+    if (visible){
+      const timer = setTimeout(() => {
+        onhide();
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [visible, onhide])
+
+  return (
+    <ToastPopup toast={visible} role="alert" aria-live="assertive">
+      {message}
+    </ToastPopup>
+  );
+};
+
+export default Toast;
